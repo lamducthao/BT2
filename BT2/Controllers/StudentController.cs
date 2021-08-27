829e42d12563f16c6003c80fe84bcb6ad5d76840
Admin <Admin@DESKTOP-S6HDA9U>
2021-08-27 13:46:17 -0700

    Add project files.

---
+++ BT2/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BT2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetInfo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetInfo(string TEN, string TUOI, string LOP)
+        {
+            ViewBag.TEN = TEN;
+            ViewBag.TUOI = TUOI;
+            ViewBag.LOP = LOP;
+            return View();
+
+        }
+    }
+}
---
+++ BT2/Controllers/demoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BT2.Models;
+
+namespace BT2.Controllers
+{
+    public class demoController : Controller
+    {
+        GiaiPhuongTrinh gpt = new GiaiPhuongTrinh();
+        // GET: demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult hello()
+        {
+            return View();
+        }
+        public ActionResult GiaiPTB1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GiaiPTB1(double hesoA, double hesoB)
+        {
+            double x = gpt.GiaiPhuongTrinhBacNhat(hesoA, hesoB);
+            ViewBag.nghiemPT = x;
+            return View();
+        }
+    }
+}
+
+    
